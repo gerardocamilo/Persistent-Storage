83cfba63ba6e78b328c33cef045a65fbfd173c45
Gerardo Camilo <gerardocamilo@MacBook-Pro-de-Gerardo.local>
2014-10-18 11:50:59 -0400

    Initial Commit

---
+++ Persisten Storage/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Persisten Storage
+//
+//  Created by Gerardo Camilo on 18/10/14.
+//  Copyright (c) 2014 ___GRCS___. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
